aaee46fe1da4014181a445f1f959c809fc97fa08
SirBokeye <42724693+SirBokeye@users.noreply.github.com>
2018-09-20 23:00:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ bot.on("message", async message =>{
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
+    cmd = cmd.toLowerCase();
 
     if(cmd === `${prefix}Hi`){
         return message.channel.send("HI!");
